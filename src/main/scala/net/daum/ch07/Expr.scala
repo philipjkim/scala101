038311a8eb2ac02db9d8affa910a8af64c77b92a
Soo Kim <sooo@daumcorp.com>
2012-06-22 11:29:09 +0900

    refactored Expr and subclasses in more OO manner

--- src/main/scala/net/daum/ch07/Expr.scala
+++ src/main/scala/net/daum/ch07/Expr.scala
@@ -1,31 +1,17 @@
 package net.daum.ch07
 
 abstract class Expr {
-  def isNumber: Boolean
-  def isSum: Boolean
-  def numValue: Int
-  def leftOp: Expr
-  def rightOp: Expr
-  def eval(e: Expr): Int = {
-    if (e.isNumber) e.numValue
-    else if (e.isSum) eval(e.leftOp) + eval(e.rightOp)
-    else error("unrecognized expression kind")
-  }
+  def eval: Int
 }
 
 class Number(n: Int) extends Expr {
-  def isNumber = true
-  def isSum = false
-  def numValue = n
-  def leftOp = error("Number.leftOp")
-  def rightOp = error("Number.rightOp")
+  def eval = n
 }
 
 class Sum(e1: Expr, e2: Expr) extends Expr {
-  def isNumber = false
-  def isSum = true
-  def numValue = error("Sum.numValue")
-  def leftOp = e1
-  def rightOp = e2
+  def eval = e1.eval + e2.eval
 }
 
+class Prod(e1: Expr, e2: Expr) extends Expr {
+  def eval = e1.eval * e2.eval
+}
