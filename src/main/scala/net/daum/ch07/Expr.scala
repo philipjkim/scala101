558be7d175c45020c5b72075e1ce4f9964fb535d
Soo Kim <sooo@daumcorp.com>
2012-06-22 13:00:40 +0900

    added eval() to Expr and extended specs

--- src/main/scala/net/daum/ch07/Expr.scala
+++ src/main/scala/net/daum/ch07/Expr.scala
@@ -1,6 +1,12 @@
 package net.daum.ch07
 
-abstract class Expr
+abstract class Expr {
+  def eval: Int = this match {
+    case Number(n) => n
+    case Sum(l, r) => l.eval + r.eval
+    case Prod(l, r) => l.eval * r.eval
+  }
+}
 case class Number(n: Int) extends Expr
 case class Sum(e1: Expr, e2: Expr) extends Expr
 case class Prod(e1: Expr, e2: Expr) extends Expr
--- src/test/scala/net/daum/ch07/ExprSpec.scala
+++ src/test/scala/net/daum/ch07/ExprSpec.scala
@@ -6,26 +6,37 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExprSpec extends Specification {
 
-  "Number.toString" should {
-    "return 'Number(n)'" in {
-      Number(3).toString must equalTo("Number(3)")
+  "Number[n]" should {
+    val n = Number(3)
+    "return 'Number(n)' as toString return value" in {
+      n.toString must equalTo("Number(3)")
+    }
+    "return n as eval return value" in {
+      n.eval must equalTo(3)
     }
   }
 
-  "Sum.toString" should {
-    "return 'Sum(e1.toString, e2.toString)'" in {
-      val sum = Sum(Number(3), Number(5))
+  "Sum[l, r]" should {
+    val sum = Sum(Number(3), Number(5))
+    "return 'Sum(l.toString, r.toString)' as toString return value" in {
       sum.toString must equalTo("Sum(Number(3),Number(5))")
     }
-  }
-
-  "Prod.toString" should {
-    "return 'Prod(e1.toString, e2.toString)'" in {
-      val sum1 = Sum(Number(3), Number(5))
-      val sum2 = Sum(Number(5), Number(3))
-      val prod = Prod(sum1, sum2);
-      prod.toString must equalTo("Prod(Sum(Number(3),Number(5)),Sum(Number(5),Number(3)))")
+    "return l.eval+r.eval as eval return value" in {
+      sum.eval must equalTo(8)
     }
   }
 
+  "Prod[l, r]" should {
+    val n1 = Number(3)
+    val n2 = Number(5)
+    val sum1 = Sum(n1, n2)
+    val sum2 = Sum(n2, n1)
+    val prod = Prod(sum1, sum2)
+    "return 'Prod(l.toString, r.toString)' as toString return value" in {
+      prod.toString must equalTo("Prod(Sum(Number(3),Number(5)),Sum(Number(5),Number(3)))")
+    }
+    "return l.eval*r.eval as eval return value" in {
+      prod.eval must equalTo(64)
+    }
+  }
 }
