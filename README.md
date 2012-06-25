d3ded03fde5b37096d7d03d2200c18c9c5c27532
Soo Kim <sooo@daumcorp.com>
2012-06-25 17:53:45 +0900

    added more for-comprehension examples

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - http://www.scala-lang.org/docu/files/ScalaByExample.pdf
 
 - http://diamondinheritance.blogspot.kr/2007/12/scala-tail-recursive-factorial.html
+
+- http://metagear.de/articles/scala-exercises/index.html
--- src/main/scala/net/daum/ch10/ForComprehenshionApp.scala
+++ src/main/scala/net/daum/ch10/ForComprehenshionApp.scala
@@ -11,4 +11,33 @@ object ForComprehenshionApp extends App {
     } yield (i, j)
   }
   println(getPrimePairs(7))
+
+  def queens(n: Int): List[List[Int]] = {
+    def isSafe(col: Int, queens: List[Int], delta: Int): Boolean = {
+      queens match {
+        case Nil => true
+        case List() => true
+        case c :: rest =>
+          c != col && math.abs(c - col) != delta && isSafe(col, rest, delta + 1)
+      }
+    }
+    def placeQueens(k: Int): List[List[Int]] =
+      if (k == 0) List(List())
+      else for {
+        queens <- placeQueens(k - 1)
+        column <- List.range(1, n + 1)
+        if isSafe(column, queens, 1)
+      } yield column :: queens
+    placeQueens(n)
+  }
+  println(queens(8))
+
+  case class Book(title: String, authors: List[String])
+  val books: List[Book] = List(
+    Book("Programming in Scala", List("Martin Odersky", "Lex Spoon", "Bill Venners")),
+    Book("Scala for the Impatient", List("Cay Horstmann")),
+    Book("Scala in Depth", List("Joshua D. Suereth")),
+    Book("Programming Scala", List("Alex Payne", "Dean Wampler")))
+  println(for (b <- books; a <- b.authors if a startsWith "Martin") yield b.title)
+  println(for (b <- books if (b.title indexOf "Program") >= 0) yield b.title)
 }
