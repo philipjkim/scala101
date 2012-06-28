2949c69e921de31d0a32facd3eadac3f0c5dd8b8
Soo Kim <sooo@daumcorp.com>
2012-06-28 13:46:26 +0900

    modified Monoid examples using implicit parameters

--- src/main/scala/net/daum/ch15/Monoid.scala
+++ src/main/scala/net/daum/ch15/Monoid.scala
@@ -4,21 +4,21 @@ abstract class Monoid[A] extends SemiGroup[A] {
   def unit: A
 }
 
-object stringMonoid extends Monoid[String] {
-  def add(x: String, y: String) = x.concat(y)
-  def unit = ""
-}
-
-object intMonoid extends Monoid[Int] {
-  def add(x: Int, y: Int) = x + y
-  def unit = 0
-}
-
 object MonoidApp extends App {
-  def sum[A](xs: List[A])(m: Monoid[A]): A =
-    if (xs.isEmpty) m.unit
-    else m.add(xs.head, sum(xs.tail)(m))
+  implicit object stringMonoid extends Monoid[String] {
+    def add(x: String, y: String) = x.concat(y)
+    def unit = ""
+  }
 
-  println(sum(List("a", "bc", "def"))(stringMonoid))
-  println(sum(List(1, 2, 3))(intMonoid))
+  implicit object intMonoid extends Monoid[Int] {
+    def add(x: Int, y: Int) = x + y
+    def unit = 0
+  }
+
+  def sum[A](xs: List[A])(implicit m: Monoid[A]): A =
+    if (xs.isEmpty) m.unit
+    else m.add(xs.head, sum(xs.tail))
+
+  println(sum(List("a", "bc", "def")))
+  println(sum(List(1, 2, 3)))
 }
